6e1b1ddc2f0e19cef0e798eeac533c6d8b0de35b
sasanqc <sasanqc@gmail.com>
2023-07-01 16:40:55 +0330

    loading state of message

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -3,12 +3,19 @@ interface ButtonProps {
   children: React.ReactNode;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   classes?: string;
+  disable?: boolean;
 }
-const Button: React.FC<ButtonProps> = ({ children, classes = "", onClick }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  disable = false,
+  classes = "",
+  onClick,
+}) => {
   return (
     <button
       className={`text-white uppercase text-[1rem] font-bold leading-relaxed tracking-widest break-words font-spaceGrotesk pb-[10px] border-b-[2px] border-b-green hover:text-green transition-colors ${classes}`}
       onClick={onClick}
+      disabled={disable}
     >
       {children}
     </button>
